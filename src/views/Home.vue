95baece72c4ca272513074810b3e7b0126316695
sunmingyu <sunmingyu601625@pwrd.com>
2020-04-01 18:50:17 +0800

    Init InWorldWeb

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class='home'>
+    <el-menu :default-active='activeName' mode='horizontal' @select="switchMenu">
+      <el-menu-item index='/news'>资讯</el-menu-item>
+      <el-menu-item index="/about">关于</el-menu-item>
+    </el-menu>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      activeName: 'news'
+    }
+  },
+  components: {},
+  activated() {
+    this.$router.push({ path: this.activeName })
+  },
+  methods: {
+    switchMenu(active) {
+      this.activeName = active
+      this.$router.push({ path: this.activeName })
+    }
   }
 }
 </script>
---
+++ src/views/News.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="news">
+    <h1>新闻资讯</h1>
+  </div>
+</template>
